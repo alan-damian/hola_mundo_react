9c0b8a8ba920c77384150285f56fbd8f5bb7899f
alan-damian <111703724+alan-damian@users.noreply.github.com>
2023-11-02 13:11:50 -0300

    Add files via upload

---
+++ ejercicios_practica/practica-1/src/components/CartaPresentacion.jsx
@@ -0,0 +1,14 @@
+
+
+const CartaPresentacion = () => {
+    return (
+        <div>
+                <h1>Alan Barragan:</h1>
+                <p>Hola soy un estudiante de programacion, actualmente poseo conocimientos 
+                de maquetado y estilos para el desarrollo web, asi como un poco de backend 
+                sigo adquiriendo conocimientos con el objetivo de ser fullstack.</p>
+        </div>
+       )
+}
+
+export default CartaPresentacion;
